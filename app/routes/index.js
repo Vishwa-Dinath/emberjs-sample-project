92f5774a67f8d4805699608430525103f1553353
Vishwa Dinath <vdinath@mitrai.com>
2024-02-14 09:25:48 +0530

    Added the search functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
-
   @service store;
 
   async model() {
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class rentalRoute extends Route {
-
   @service store;
 
   async model(params) {
